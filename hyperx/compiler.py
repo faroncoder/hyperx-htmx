c54befc20f9a8601ba71ffb941e9b96919a9eef3
faroncoder <jeff.panasuik@gmail.com>
2025-10-07 19:54:04 -0400

    repairing lint issue

--- hyperx/compiler.py
+++ hyperx/compiler.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 from dataclasses import dataclass, field
 from typing import Dict, List, Optional
 import json
-
+from hyperx.templatetags.hyperx import convert_import
 
 # ─────────────────────────────────────────────
 # 📦 AST Structures
